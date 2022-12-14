1cc31b843d06e68a33c8876a1167c8316c35b938
Roshan Kumar Sharma <roshan.sharma@deprontoinfotech.com>
2022-12-14 23:10:57 +0530

    * Layout Completed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { Breadcrumb, Layout, Menu, theme, ConfigProvider } from "antd"
+import { HomeOutlined, FormOutlined,BookOutlined, EditOutlined, MailOutlined, UnorderedListOutlined,RadarChartOutlined,TeamOutlined,StarOutlined, RiseOutlined,CalendarOutlined } from  "@ant-design/icons"
+import { useState } from "react"
 
 function App() {
+  const { Header, Content, Footer, Sider } = Layout
+  function getItem(label, key, icon, children, type) {
+    return {
+      key,
+      icon,
+      children,
+      label,
+      type,
+    }
+  }
+  const items = [
+    getItem("Home","1",<HomeOutlined/>),
+    getItem("My Content", "2",<FormOutlined />),
+    getItem("My Workbook", "3",<BookOutlined />),
+    getItem("Create", "4",<EditOutlined />),
+    getItem("Email List", "5",<MailOutlined />),
+    getItem("Todo List", "6",<UnorderedListOutlined />),
+    getItem("Analytics", "7",<RadarChartOutlined />),
+    getItem("Trending", "8", <RiseOutlined />),
+    getItem("Reminder", "9",<CalendarOutlined />),
+    getItem("Friends", "10",<TeamOutlined />),
+    getItem("BookMarks", "11",<StarOutlined />),
+  ]
+
+  const [collapsed, setCollapsed] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#00b96b",
+          },
+        }}
+      >
+        <Layout style={{ minHeight: "100vh" }}>
+          <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+            <div
+              style={{
+                height: 32,
+                margin: 16,
+                background: "rgba(255, 255, 255, 0.2)",
+              }}
+            />
+            <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline" items={items} />
+          </Sider>
+        </Layout>
+      </ConfigProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
